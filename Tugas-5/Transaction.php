19d83856dc5237f466ebe0c2a76866743c2a9de7
anisapobela10 <anisapbl10@gmail.com>
2026-09-26 07:55:25 -0700

    Tambah proses transaksi deposit dan penarikan

--- Tugas-5/Transaction.php
+++ Tugas-5/Transaction.php
@@ -10,7 +10,59 @@ class Transaction
         private string $type,
         private float $amount
     ){
+    }
+    public function process(): bool
+    {
+
+        if(session_status() === PHP_SESSION_NONE){
+
+            session_start();
+
+        }
+
+
+        if(!isset($_SESSION['balance'])){
+
+            $_SESSION['balance'] = 0;
+
+        }
+
+
+        $result = match($this->type){
+
+            "deposit" => function(){
+
+                $_SESSION['balance'] += $this->amount;
+
+                return true;
+
+            },
+
+
+            "withdraw" => function(){
+
+                if($_SESSION['balance'] < $this->amount){
+
+                    return false;
+
+                }
+
+
+                $_SESSION['balance'] -= $this->amount;
+
+                return true;
+
+            },
+
+
+            default => false
+
+        };
+
+
+        return $result();
 
     }
+        
 
 }
