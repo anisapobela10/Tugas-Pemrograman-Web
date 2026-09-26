897fb830c136a91a0c33be3ee9bf7b0cefd95e92
anisapobela10 <anisapbl10@gmail.com>
2026-09-26 07:47:30 -0700

    Tambah class Transaction dengan constructor promotion

--- Tugas-5/Transaction.php
+++ Tugas-5/Transaction.php
@@ -5,4 +5,12 @@ declare(strict_types=1);
 class Transaction
 {
 
+    public function __construct(
+        private string $id,
+        private string $type,
+        private float $amount
+    ){
+
+    }
+
 }
